8b1e3f28a026f02c095e1e0296ac880f0083971a
igunsaya <igunsaya@gmail.com>
2019-06-21 10:34:14 +0100

    Revert name change

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -280,7 +280,7 @@ module.exports = function(webpackEnv) {
       splitChunks: bpkReactScriptsConfig.enableAutomaticChunking
         ? {
             chunks: 'all',
-            name: isEnvDevelopment,
+            name: false,
             cacheGroups: bpkReactScriptsConfig.vendorsChunkRegex ? {
               vendors: {
                 test: new RegExp(bpkReactScriptsConfig.vendorsChunkRegex),
